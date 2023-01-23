05a83e5d8609cdb496dc17106f449e192f9c81f0
BuildTools <zrdenning@gmail.com>
2023-01-22 17:21:11 -0800

    Added Kubryzlt Keys

--- src/main/java/io/github/eisoptrophobia/kubryzltcraft/block/BlockKubryzlt.java
+++ src/main/java/io/github/eisoptrophobia/kubryzltcraft/block/BlockKubryzlt.java
@@ -1,18 +1,53 @@
 package io.github.eisoptrophobia.kubryzltcraft.block;
 
 import io.github.eisoptrophobia.kubryzltcraft.block.entity.ModTileEntities;
+import io.github.eisoptrophobia.kubryzltcraft.block.entity.TileEntityEdificeCore;
+import io.github.eisoptrophobia.kubryzltcraft.data.capability.kubryzltcraftteammember.CapabilityKubryzltcraftTeamMember;
+import io.github.eisoptrophobia.kubryzltcraft.item.ModItems;
+import io.github.eisoptrophobia.kubryzltcraft.warfare.Territory;
+import io.github.eisoptrophobia.kubryzltcraft.warfare.TerritoryManager;
+import io.github.eisoptrophobia.kubryzltcraft.warfare.team.KubryzltcraftTeam;
+import io.github.eisoptrophobia.kubryzltcraft.warfare.team.KubryzltcraftTeamManager;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class BlockKubryzlt extends Block {
 	
 	public BlockKubryzlt() {
 		super(AbstractBlock.Properties.of(Material.METAL).strength(-1.0F, 3600000.0F).lightLevel(s -> 15));
+	}
+	
+	@Override
+	public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult trace) {
+		if (player.getItemInHand(hand).getItem() == ModItems.KUBRYZLT_KEY.get()) {
+			if (!world.isClientSide()) {
+				if (!player.isCrouching()) {
+					Territory territory = TerritoryManager.getManager().getTerritoryByBlockPos(world, pos);
+					player.getCapability(CapabilityKubryzltcraftTeamMember.INSTANCE).ifPresent(handler -> {
+						KubryzltcraftTeam team = handler.getTeam();
+						if (team != KubryzltcraftTeamManager.getTerritoryTeamServer(territory)) {
+							TerritoryManager.getManager().convert(territory, team);
+							player.getItemInHand(hand).shrink(1);
+						}
+					});
+				}
+			}
+		}
+		return ActionResultType.SUCCESS;
 	}
 	
 	@Override
--- src/main/java/io/github/eisoptrophobia/kubryzltcraft/item/ModItems.java
+++ src/main/java/io/github/eisoptrophobia/kubryzltcraft/item/ModItems.java
@@ -3,6 +3,7 @@ package io.github.eisoptrophobia.kubryzltcraft.item;
 import io.github.eisoptrophobia.kubryzltcraft.Kubryzltcraft;
 import io.github.eisoptrophobia.kubryzltcraft.item.creative.ItemGroupKubryzltcraft;
 import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,8 @@ public class ModItems {
 	public static final RegistryObject<Item> BLUEPRINT_BLANK = ITEMS.register("blueprint_blank", () -> new Item(new Item.Properties().tab(ItemGroupKubryzltcraft.INSTANCE)));
 	
 	public static final RegistryObject<Item> BLUEPRINT_RADAR = ITEMS.register("blueprint_radar", () -> new Item(new Item.Properties().tab(ItemGroupKubryzltcraft.INSTANCE)));
+	
+	public static final RegistryObject<Item> KUBRYZLT_KEY = ITEMS.register("kubryzlt_key", () -> new Item(new Item.Properties().tab(ItemGroupKubryzltcraft.INSTANCE).rarity(Rarity.RARE)));
 	
 	public static void register(IEventBus bus) {
 		ITEMS.register(bus);
